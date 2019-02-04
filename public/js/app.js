3c49d113d845334d6f6cc6272a6da988b5679b9c
Stepan Kiryanenko <stepankiryanenko@gmail.com>
2019-02-04 16:25:17 +0200

    create records page

--- public/js/app.js
+++ public/js/app.js
@@ -48106,6 +48106,17 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./resources/sass/main.scss":
+/*!**********************************!*\
+  !*** ./resources/sass/main.scss ***!
+  \**********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ "./resources/sass/style.scss":
 /*!***********************************!*\
   !*** ./resources/sass/style.scss ***!
@@ -48118,15 +48129,16 @@ __webpack_require__.r(__webpack_exports__);
 /***/ }),
 
 /***/ 0:
-/*!*****************************************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/style.scss ***!
-  \*****************************************************************************************/
+/*!********************************************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/style.scss ./resources/sass/main.scss ***!
+  \********************************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! /home/user/projects/webpack/resources/js/app.js */"./resources/js/app.js");
 __webpack_require__(/*! /home/user/projects/webpack/resources/sass/app.scss */"./resources/sass/app.scss");
-module.exports = __webpack_require__(/*! /home/user/projects/webpack/resources/sass/style.scss */"./resources/sass/style.scss");
+__webpack_require__(/*! /home/user/projects/webpack/resources/sass/style.scss */"./resources/sass/style.scss");
+module.exports = __webpack_require__(/*! /home/user/projects/webpack/resources/sass/main.scss */"./resources/sass/main.scss");
 
 
 /***/ })
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,10 @@
+<hr>
+<footer>
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 text-center">
+				<p class="copyright text-muted">Copyright &copy; Your Website 2019</p>
+			</div>
+		</div>
+	</div>
+</footer>
